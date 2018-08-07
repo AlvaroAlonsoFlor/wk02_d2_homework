402bd3bad1ed8c53b547959aa1443d88006959d4
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-07 16:08:06 +0100

    Implement all bear methods

--- bear.rb
+++ bear.rb
@@ -6,4 +6,19 @@ class Bear
     @stomach = stomach
   end
 
+  def roar
+    return "Grr!"
+  end
+
+  def count_food
+    food_eaten = @stomach.count
+
+    if @stomach == []
+      return "The bear hasn't eaten any food recently"
+    else
+      return "The bear has eaten #{food_eaten} fishes"
+    end
+
+  end
+
 end
--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -4,14 +4,28 @@ require_relative("../bear.rb")
 
 class BearTest < MiniTest::Test
   def setup
-    @bear = Bear.new("Brownie")
+    @bear1 = Bear.new("Brownie")
+    @bear2 = Bear.new("Blackie", ["fish1", "fish2"])
   end
 
   def test_has_a_name
-    assert_equal("Brownie",@bear.name)
+    assert_equal("Brownie",@bear1.name)
   end
 
   def test_has_stomach
-    assert_equal([], @bear.stomach)
+    assert_equal([], @bear1.stomach)
+  end
+
+  def test_can_roar
+    assert_equal("Grr!", @bear1.roar)
+  end
+
+  def test_food_count_is_empty
+    assert_equal("The bear hasn't eaten any food recently", @bear1.count_food)
+  end
+
+  def test_food_count
+    expected = "The bear has eaten 2 fishes"
+    assert_equal(expected, @bear2.count_food)
   end
 end
